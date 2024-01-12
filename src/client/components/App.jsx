cea3c36cc4e6a9d0829f074cbde4cc5cf5dc9cba
fletcher burton <burton.fletcher@aol.com>
2024-01-12 11:24:16 -0600

    commit2

---
+++ src/client/components/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+import Login from './components/Login';
+
+import MyLogoComponent from './components/MyLogoComponent';
+
+function App() {
+
+
+  return (
+    <div className='App'>
+      
+        <Login />
+        <MyLogoComponent />
+    </div>
+  );
+}
+
+export default App;
